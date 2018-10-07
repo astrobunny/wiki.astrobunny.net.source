36be8ddc5facf56074f315fea341c6cc25234e0f
David Siaw <davidsiaw@gmail.com>
2018-10-07 23:43:25 +0900

    add upload commands

--- deploy.bash
+++ deploy.bash
@@ -3,9 +3,12 @@ set -e
 git config --global user.email "davidsiaw@gmail.com"
 git config --global user.name "David Siaw (via Circle CI)"
 
+docker create -v /app/data -v /app/images --name sources alpine:3.4 /bin/true
+docker cp data sources:/app/data
+docker cp images sources:/app/images
 git clone git@github.com:davidsiaw/hibiol.davidsiaw.net.git build
 cp -r build/.git ./gittemp
-docker run -v `pwd`/build:/app/release -v `pwd`/data:/app/data -v `pwd`/images:/app/images --name builder davidsiaw/hibiol build
+docker run --volumes-from sources --name builder davidsiaw/hibiol build
 rm -rf build
 docker cp -f builder:/app/release build
 cp -r ./gittemp build/.git
