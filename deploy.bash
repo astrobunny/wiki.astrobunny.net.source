6534dbf797288dfea024e9b58779e45635baa789
David Siaw <davidsiaw@gmail.com>
2018-10-07 23:47:29 +0900

    technicalities

--- deploy.bash
+++ deploy.bash
@@ -4,13 +4,13 @@ git config --global user.email "davidsiaw@gmail.com"
 git config --global user.name "David Siaw (via Circle CI)"
 
 docker create -v /app/data -v /app/images --name sources alpine:3.4 /bin/true
-docker cp data sources:/app/data
-docker cp images sources:/app/images
+docker cp data/. sources:/app/data
+docker cp images/. sources:/app/images
 git clone git@github.com:davidsiaw/hibiol.davidsiaw.net.git build
 cp -r build/.git ./gittemp
 docker run --volumes-from sources --name builder davidsiaw/hibiol build
 rm -rf build
-docker cp -f builder:/app/release build
+docker cp builder:/app/release build
 cp -r ./gittemp build/.git
 pushd build
 echo hibiol.davidsiaw.net > CNAME
