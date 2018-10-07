9c68bb180439b5facba1616c55b59471edfcfe2d
David Siaw <davidsiaw@gmail.com>
2018-10-07 23:38:34 +0900

    try this again

--- deploy.bash
+++ deploy.bash
@@ -5,7 +5,9 @@ git config --global user.name "David Siaw (via Circle CI)"
 
 git clone git@github.com:davidsiaw/hibiol.davidsiaw.net.git build
 cp -r build/.git ./gittemp
-docker run -v `pwd`/build:/app/release -v `pwd`/data:/app/data -v `pwd`/images:/app/images -ti davidsiaw/hibiol build
+docker run -v `pwd`/build:/app/release -v `pwd`/data:/app/data -v `pwd`/images:/app/images --name builder davidsiaw/hibiol build
+rm -rf build
+docker cp -f builder:/app/release build
 cp -r ./gittemp build/.git
 pushd build
 echo hibiol.davidsiaw.net > CNAME
@@ -15,4 +17,4 @@ git add .
 git add -u
 git commit -m "update `date`"
 ssh-agent bash -c 'ssh-add ~/.ssh/id_github.com; git push'
-popd
+          
